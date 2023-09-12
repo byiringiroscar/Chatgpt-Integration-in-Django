283090533b0707dc77a41083586ab987a3f234f7
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-09-12 14:57:16 +0200

    Update post to openai

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,12 +1,30 @@
+import openai
 from django.shortcuts import render
 from django.http import JsonResponse
+from decouple import config
+import openai
 
 # Create your views here.
+openai_api_key = config('openai_api_key')
+openai.api_key = openai_api_key
+
+
+def ask_openai(message):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=message,
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.7
+    )
+    answer = response.choices[0].text.strip()
+    return answer
 
 
 def chatbot(request):
     if request.method == 'POST':
         message = request.POST.get('message')
-        response = "Hi this is my  response"
+        response = ask_openai(message)
         return JsonResponse({'message': message, 'response': response})
     return render(request, 'templates/chatbot.html')
