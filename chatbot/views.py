9f874654d503931e789c4dbdd25f7f6b82fa38bf
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-09-12 17:47:55 +0200

    Save the data in database for chat

--- chatbot/views.py
+++ chatbot/views.py
@@ -27,13 +27,14 @@ def ask_openai(message):
 
 
 def chatbot(request):
+    chats = Chat.objects.filter(user=request.user)
     if request.method == 'POST':
         message = request.POST.get('message')
         response = ask_openai(message)
         chat = Chat(user=request.user, message=message, response=response, created_at=timezone.now())
         chat.save()
         return JsonResponse({'message': message, 'response': response})
-    return render(request, 'templates/chatbot.html')
+    return render(request, 'templates/chatbot.html', {'chats': chats})
 
 
 def login(request):
